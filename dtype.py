fcd85bb2b3711792a86ca96d50490f6830b3af8a
hitgalani <hitgalani@gmail.com>
2026-08-01 22:15:54 +0530

    add user_input file and add data_type file

---
+++ dtype.py
@@ -0,0 +1,12 @@
+name = "Hello"
+age = 1
+cgpe = 5.5
+is_student = True
+com = 1 + 5.5j
+
+
+print(type(name))
+print(type(age))
+print(type(cgpe))
+print(type(is_student))
+print(type(com))
---
+++ use_input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+cgpe = float(input("Enter your CGPE: "))
+
+
+print("Your name is:", name)
+print("Your age is:", age)
+print("Your CGPE is:", cgpe)
+
+print(type(name))
+print(type(age))
+print(type(cgpe))
