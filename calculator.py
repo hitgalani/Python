cc7ac9a9b9d978c5ad9f3843c87e9588e9b45527
hitgalani <hitgalani@gmail.com>
2026-08-01 23:05:07 +0530

    calculator creat useing user input and oprater

---
+++ calculator.py
@@ -0,0 +1,37 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operator = input("Enter operator (+, -, *, /, %, //, **): ")
+
+if operator == "+":
+    print("Result =", num1 + num2)
+
+elif operator == "-":
+    print("Result =", num1 - num2)
+
+elif operator == "*":
+    print("Result =", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Error! Division by zero.")
+
+elif operator == "%":
+    if num2 != 0:
+        print("Result =", num1 % num2)
+    else:
+        print("Error! Division by zero.")
+
+elif operator == "//":
+    if num2 != 0:
+        print("Result =", num1 // num2)
+    else:
+        print("Error! Division by zero.")
+
+elif operator == "**":
+    print("Result =", num1 ** num2)
+
+else:
+    print("Invalid Operator!")
